d90ad9b6fef0ea7fc447add853d65d75a92b25f2
Obi-Bram Keno Beer <gizmo_xxl@hotmail.com>
2018-03-11 14:47:29 +0100

    add filter by album option

--- js/main.js
+++ js/main.js
@@ -45,6 +45,7 @@ let myPicturesArray = [{
 // cache relevant page parts in variables
 const imagesWrapper = document.querySelector('#thumbnails');
 const largeImage = document.querySelector('#large-image img');
+const filter = document.querySelector('#filters');
 
 // function to display a large image
 function showImage(imageURL, imageTitle){
@@ -56,36 +57,41 @@ function showImage(imageURL, imageTitle){
 showImage(myPicturesArray[0].url, myPicturesArray[0].title);
 
 // function to render the image thumbnails
-function renderImages() {
+function renderImages(album) {
+    imagesWrapper.innerHTML = "";
     myPicturesArray.forEach(function (currentImage) {
 
-        // create the image item
-        let imageItem = document.createElement("div");
-        imageItem.classList.add('thumbnail');
+        if (album == undefined || album == "all" || album == currentImage.albumId) {
+            // create the image item
+            let imageItem = document.createElement("div");
+            imageItem.classList.add('thumbnail');
 
-        // create the image
-        let image = document.createElement("img");
-        image.src = currentImage.thumbnailUrl;
-        image.alt = currentImage.title;
-        image.setAttribute('data-id', currentImage.id);
-        image.setAttribute('data-album', currentImage.albumId);
-        image.addEventListener('click', function () {
-            showImage(currentImage.url, currentImage.title);
-        });
+            // create the image
+            let image = document.createElement("img");
+            image.src = currentImage.thumbnailUrl;
+            image.alt = currentImage.title;
+            image.setAttribute('data-id', currentImage.id);
+            image.setAttribute('data-album', currentImage.albumId);
+            image.addEventListener('click', function () {
+                showImage(currentImage.url, currentImage.title);
+            });
 
-        // create the delete button
-        let deleteButton = document.createElement("button");
-        deleteButton.innerText = 'X';
-        deleteButton.addEventListener('click', function () {
-            deleteImage(this, currentImage.id);
-        });
+            // create the delete button
+            let deleteButton = document.createElement("button");
+            deleteButton.innerText = 'X';
+            deleteButton.addEventListener('click', function () {
+                deleteImage(this, currentImage.id);
+            });
 
-        // append image and button to item wrapper
-        imageItem.append(image);
-        imageItem.append(deleteButton);
+            // append image and button to item wrapper
+            imageItem.append(image);
+            imageItem.append(deleteButton);
 
-        // append image item to images wrapper
-        imagesWrapper.append(imageItem);
+            // append image item to images wrapper
+            imagesWrapper.append(imageItem);
+
+        }
+
     });
 }
 
@@ -101,25 +107,13 @@ function deleteImage(element, imageID){
     for(i=0; i<myPicturesArray.length; i++){
         if (myPicturesArray[i].id === imageID){
             myPicturesArray.splice(i, 1);
+            break;
         }
     }
 }
 
-/* ASIGNMENTS
-1.	Improve the display, by adding margins (CSS), shadows, border, and changing the URLs for real pictures.
-Remember that you need to have a smaller versions of the pictures, i.e., thumbnails.
-You can use existing images (images.google.com is your friend) or images you upload somewhere.
-
-2. Use JavaScript for adding a click event listener on each image, then when clicked, you will show a bigger version of each picture.
-For the moment, just change the value of the src attribute of the clicked image (set it to the URL of the full size image from the array).
-
-3. Try to make something nicer: reserve a <div> on the right of the document so to display the clicked image with a bigger size.
-In that case, you will need to create an image (only once, after the first click), to set it to the size of the div (use the width and height attributes of the img element), and to append it to the div.
-
-4. Try to add more images, and find a way to display them per album.
-Create buttons entitled "album1", "album2", etc., and when clicked, you will only display images from the selected album.
-
-5. [Advanced] Add an option for deleting a picture. It should be removed from the document and from the array too...
-
-6. Feel free to add any interesting feature you think about ;-)
-*/
+// function to filter images per album
+filter.onchange = function() {
+    let selectedOption = filter.options[filter.selectedIndex].value;
+    renderImages(selectedOption);
+}
